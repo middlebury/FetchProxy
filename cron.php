ac555204090da3a3a26d989438d59a701d90a9e1
Adam Franco <afranco@middlebury.edu>
2011-10-17 13:45:46 -0400

    Feeds older than MAX_LIFE_WITHOUT_ACCESS are now deleted and fetching is stopped.
    This prevents unneeded feeds from continuing to take up fetching time long after
    they are no longer needed.

--- cron.php
+++ cron.php
@@ -4,6 +4,7 @@ require_once('config.php');
 require_once('lib.php');
 $db = new PDO(DB_DSN, DB_USER, DB_PASS);
 
+// Update our feeds
 $stmt = $db->prepare(
 	'SELECT id, url
 	FROM feeds 
@@ -16,4 +17,28 @@ $rows = $stmt->fetchAll(PDO::FETCH_OBJ);
 
 foreach ($rows as $row) {
 	fetch_url($row->id, $row->url);
+}
+
+// Delete feeds that haven't been accessed in a very long time so we don't waste
+// effort continuing to fetch them.
+$stmt = $db->prepare(
+	'SELECT id, url
+	FROM feeds 
+	WHERE 
+		(last_access < DATE_SUB(NOW(), INTERVAL '.MAX_LIFE_WITHOUT_ACCESS.'))
+	');
+$stmt->execute();
+$rows = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+$logStmt = $db->prepare('INSERT INTO log (feed_id, feed_url, message) VALUES (:feed_id, :feed_url, :message)');
+$deleteStmt = $db->prepare('DELETE FROM feeds WHERE id = ?');
+foreach ($rows as $row) {
+	$db->beginTransaction();
+	$logStmt->execute(array(
+		':feed_id' => $row->id,
+		':feed_url' => $row->url,
+		':message' => 'Feed not accessed in '.MAX_LIFE_WITHOUT_ACCESS.', deleting.',
+	));
+	$deleteStmt->execute(array($row->id));
+	$db->commit();
 }
