7bddf94c96f4551413564cf219f4dca7a7d6cf98
Ian McBride <imcbride@middlebury.edu>
2019-08-02 15:13:59 -0400

    Handle the exception case where the response is null.

--- lib.php
+++ lib.php
@@ -81,7 +81,11 @@ function fetch_url ($id, $url) {
 		}
 	}
 	catch (TooManyRedirectsException $e) {
-		fetch_error($id, $url, $res->getHeaderLine('X-Guzzle-Redirect-History'), 'Redirect limit of ' . FETCH_MAX_REDIRECTS . ' exceeded. ' . $res->getHeaderLine('X-Guzzle-Redirect-History'), 552, 'Too Many Redirects');
+		try {
+			fetch_error($id, $url, $res->getHeaderLine('X-Guzzle-Redirect-History'), 'Redirect limit of ' . FETCH_MAX_REDIRECTS . ' exceeded. ' . $res->getHeaderLine('X-Guzzle-Redirect-History'), 552, 'Too Many Redirects');
+		} catch (Exception $e) {
+			fetch_error($id, $url, NULL, get_class($e).': '.$e->getMessage(), 550, 'HTTP Error');
+		}
 	}
 	catch (Exception $e) {
 		fetch_error($id, $url, NULL, get_class($e).': '.$e->getMessage(), 550, 'HTTP Error');
